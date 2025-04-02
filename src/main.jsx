70188f92da799dd0ce62fb57a8398c4e25b6c445
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-02 13:33:30 +0530

    created the TanStack Query Object and use QueryClientProvider to provide it in our app

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={QueryClient}>
+      <App />
+    </QueryClientProvider>
+  </StrictMode>
+);
