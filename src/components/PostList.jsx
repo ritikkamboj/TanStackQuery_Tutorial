8b9e774916cf14659767491a047fe0c0dc9a4422
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-02 20:48:05 +0530

    useQuery to fetch the tags data and set on UI

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,28 +1,58 @@
 import { useMutation, useQuery } from "@tanstack/react-query";
 import React from "react";
-import { addPost, fetchPosts } from "../api/api";
+import { addPost, fetchPosts, fetchTags } from "../api/api";
 
 function PostList() {
-  const { data: postData, isLoading, isError, error } = useQuery({
+  const {
+    data: postData,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
     queryKey: ["posts"],
     queryFn: fetchPosts,
   });
 
-  const {mutate, isError : isPostError, error: postError , isPending, reset} = useMutation({
-    mutationFn : addPost,
+  const {data: tagsData} = useQuery({
+    queryKey : ['tags'],
+    queryFn : fetchTags
+  })
+
+
+  const {
+    mutate,
+    isError: isPostError,
+    error: postError,
+    isPending,
+    reset,
+  } = useMutation({
+    mutationFn: addPost,
   });
 
   console.log(postData, isLoading, isError, error);
   return (
     <div className="container">
-      
+      <form action="">
+        <input type="text" placeholder="Enter Your Text..." name="title" />
+
+<div className="tags">
+{tagsData?.map((tags)=> <div>
+            <input type="checkbox" name="tag" id="tag" />
+            <label htmlFor="tag">{tags}</label>
+           
+        </div>)}
+</div>
+
        
+        <button>Post</button>
+      </form>
+
       {isLoading && <p>loading...</p>}
       {isError && <p>{error.message}</p>}
       {postData?.map((post) => {
         return (
           <div key={post.id} className="post">
-           <div> {post.title}</div>
+            <div> {post.title}</div>
             {post.tags.map((tags) => (
               <span key={tags}>{tags}</span>
             ))}
