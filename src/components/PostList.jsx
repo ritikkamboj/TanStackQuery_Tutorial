ef1f4d8d5ede2c6717d3b31319f7c7194e10c68c
Aashish_kumar2 <ritikkamboj4314@gmail.com>
2025-04-03 23:42:14 +0530

    dealing with form submit and getting formObhect data

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -28,10 +28,23 @@ function PostList() {
     mutationFn: addPost,
   });
 
-  console.log(postData, isLoading, isError, error);
+  console.log(postData, isLoading, isError, error); 
+
+  const handleSubmit = (e) =>
+  {
+    e.preventDefault();
+    console.log(e.target)
+    // console.log('jai baabe ki')
+    const formData = new FormData(e.target);
+    console.log(formData)
+    const title = formData.get('title');
+// console.log(Array.from(formData.keys()))
+const tags =Array.from(formData.keys()).filter((key)=> formData.get(key)==='on');
+console.log(tags)
+  }
   return (
     <div className="container">
-      <form action="">
+      <form action="" onSubmit={handleSubmit}>
         <input type="text" placeholder="Enter Your Text..." name="title" className="postbox" />
 
         <div className="tags">
